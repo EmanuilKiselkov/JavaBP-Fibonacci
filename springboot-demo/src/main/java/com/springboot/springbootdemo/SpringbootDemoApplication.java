c4f8a7ffeccbd5720d676469b31fd6b9f6dfae95
EmanuilKiselkov <117589358+EmanuilKiselkov@users.noreply.github.com>
2023-05-14 13:52:50 +0300

    Initial commit

---
+++ springboot-demo/src/main/java/com/springboot/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+		System.out.println("spring boot hello");
+	}
+
+
+}
